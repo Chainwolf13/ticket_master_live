cb25c5aed3018436a1366279daced5cf83c6bff4
iamjah123 <jahmaro2@gmail.com>
2023-12-14 16:35:10 -0500

    Hello Repository

---
+++ ticket_master_live/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for TicketMasterFinalProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from ticketlive import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('accounts/', include("django.contrib.auth.urls")),
+    # path('register', include("django.contrib.auth.urls"), name='register'),
+    path('register', views.register_view, name='register'),
+    path('logInPage.html', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('cart_add', views.cart_add, name='cart_add'),
+    path('cart.html', views.cart_view, name='cart_view'),
+    path('delete_cart/<str:id>', views.delete_cart, name='delete_cart'),
+    path('update_cart/<str:id>', views.update_cart, name='update_cart'),
+
+]
